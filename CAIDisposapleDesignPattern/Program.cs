ea2046d3d0ccb32074c33eba73d38d7016109e0b
unknown <abdallahaliahmed517@gmail.com>
2023-08-15 15:57:13 +0300

    ss

--- CAIDisposapleDesignPattern/Program.cs
+++ CAIDisposapleDesignPattern/Program.cs
@@ -5,7 +5,33 @@ using CAIDisposapleDesignPattern;
 
 // 1) not recommended
 
-CurrencyService currencyService = new CurrencyService();
-var result = currencyService.GetCurrencies();
+//CurrencyService currencyService = new CurrencyService();
+//var result = currencyService.GetCurrencies();
 //currencyService.Dispose();
-Console.WriteLine(result);
+//Console.WriteLine(result);
+
+//2) recommended
+//CurrencyService currencyService = null;
+//try
+//{
+//    currencyService = new CurrencyService();
+//    var result = currencyService.GetCurrencies();
+//    Console.WriteLine(result);
+
+//}
+//catch (Exception)
+//{
+//    Console.WriteLine("Error");
+//}
+//finally
+//{
+//    currencyService?.Dispose(); 
+//}
+
+// 3) more recommended  using .net framework 2+
+
+using (CurrencyService currencyService = new CurrencyService())
+{ 
+    var result = currencyService.GetCurrencies();
+    Console.WriteLine(result);
+}
