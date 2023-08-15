32f02271fed9aca799ea5305c582f385c6cf7dde
unknown <abdallahaliahmed517@gmail.com>
2023-08-15 15:32:53 +0300

    ss

---
+++ CAIDisposapleDesignPattern/CurrencyService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAIDisposapleDesignPattern
+{
+    public class CurrencyService
+    {
+        private readonly HttpClient httpClient;
+        public CurrencyService()
+        {
+            
+        }
+        public string GetCurrencies()
+        {
+            string url = "https://coinbase.com/api/v2/currencies";
+            //var result = httpClient.GetStringAsync(url).Result;
+            //return result;
+        }
+    }
+}
