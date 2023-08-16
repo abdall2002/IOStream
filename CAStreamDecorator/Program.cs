c3904c2370f1e311e2bd1b1165f12fc00e49b0c0
unknown <abdallahaliahmed517@gmail.com>
2023-08-16 14:32:04 +0300

    ss

--- CAStreamDecorator/Program.cs
+++ CAStreamDecorator/Program.cs
@@ -1,11 +1,28 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using System;
+using System.IO;
+using System.IO.Compression;
 
+using (var stream = File.Create("data.bin"))
+{
+    using (var ds = new DeflateStream(stream, CompressionMode.Compress))
+    {
+        ds.WriteByte(65);
+        ds.WriteByte(66);
+    }
+}
 
-
-
-
+using (var stream = File.OpenRead("data.bin"))
+{
+    using (var ds = new DeflateStream(stream, CompressionMode.Decompress))
+    {
+        for (int i = 0; i < stream.Length; i++)
+        {
+            Console.WriteLine(ds.ReadByte());
+        }
+    }
+}
 
 
 
