ec0e9296f0eace91f1eb232212225b34db4d4c55
unknown <abdallahaliahmed517@gmail.com>
2023-08-16 12:25:43 +0300

    ss

--- CAFileStream/Program.cs
+++ CAFileStream/Program.cs
@@ -10,17 +10,19 @@ static void Example01()
 {
     //string path = "C:\\Users\\COMPUMARTS\\Desktop\\Sample.txt";
     string path = "C:\\Users\\COMPUMARTS\\Desktop\\Sample.txt";
-    using (var fs = new FileStream(path, FileMode.Open, FileAccess.Read))
+    using (var fs = new FileStream(path, FileMode.Open, FileAccess.ReadWrite))
     {
         Console.WriteLine($"Length: {fs.Length} Byte(s)");
         Console.WriteLine($"CanRead: {fs.CanRead}");
         Console.WriteLine($"CanWrite: {fs.CanWrite}");
         Console.WriteLine($"CanSeek: {fs.CanSeek}");
         Console.WriteLine($"CanTimeout: {fs.CanTimeout}");
-        Console.WriteLine($"Position: {fs.Position}");
-        //fs.WriteByte(65); // A
-        //Console.WriteLine($"Position: {fs.Position}");
-        //fs.WriteByte(66); // B
+        Console.WriteLine($"Position: {fs.Position}");     // print Position: 0
+        fs.WriteByte(65); // A
+        Console.WriteLine($"Position: {fs.Position}");     // print Position: 1
+        fs.WriteByte(66); // B
+        Console.WriteLine($"Position: {fs.Position}");     // print Position: 2
+
         //fs.WriteByte(13); // Enter
 
         //for (byte i = 65; i < 123; i++)
