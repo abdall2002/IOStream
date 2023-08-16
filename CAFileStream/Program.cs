68a418d024f9012ca8c219af1a4d6883084de09a
unknown <abdallahaliahmed517@gmail.com>
2023-08-16 12:13:01 +0300

    add

--- CAFileStream/Program.cs
+++ CAFileStream/Program.cs
@@ -1,15 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System;
+using System.IO;
+
 Example01();
 
 
-
-
-void Example01()
+static void Example01()
 {
-    string path = "C:\\Users\\COMPUMARTS\\Desktop";
-    //string path = @"C:\Users\COMPUMARTS\Desktop";
-    using (var fs = new FileStream(path, FileMode.Open, FileAccess.ReadWrite))
+    //string path = "C:\\Users\\COMPUMARTS\\Desktop\\Sample.txt";
+    string path = "C:\\Users\\COMPUMARTS\\Desktop\\Sample.txt";
+    using (var fs = new FileStream(path, FileMode.Open, FileAccess.Read))
     {
         Console.WriteLine($"Length: {fs.Length} Byte(s)");
         Console.WriteLine($"CanRead: {fs.CanRead}");
@@ -22,10 +23,10 @@ void Example01()
         //fs.WriteByte(66); // B
         //fs.WriteByte(13); // Enter
 
-        for (byte i = 65; i < 123; i++)
-        {
-            fs.WriteByte(i);
-        }
+        //for (byte i = 65; i < 123; i++)
+        //{
+        //    fs.WriteByte(i);
+        //}
     }
 
 }
