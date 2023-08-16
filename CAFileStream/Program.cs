fa943b854508055c62cb01225ca2b9f5c7979811
unknown <abdallahaliahmed517@gmail.com>
2023-08-16 11:19:19 +0300

    add

---
+++ CAFileStream/Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Example01();
+
+
+
+
+void Example01()
+{
+    string path = "C:\\Users\\COMPUMARTS\\Desktop";
+    //string path = @"C:\Users\COMPUMARTS\Desktop";
+    using (var fs = new FileStream(path, FileMode.Open, FileAccess.ReadWrite))
+    {
+        Console.WriteLine($"Length: {fs.Length} Byte(s)");
+        Console.WriteLine($"CanRead: {fs.CanRead}");
+        Console.WriteLine($"CanWrite: {fs.CanWrite}");
+        Console.WriteLine($"CanSeek: {fs.CanSeek}");
+        Console.WriteLine($"CanTimeout: {fs.CanTimeout}");
+        Console.WriteLine($"Position: {fs.Position}");
+        //fs.WriteByte(65); // A
+        //Console.WriteLine($"Position: {fs.Position}");
+        //fs.WriteByte(66); // B
+        //fs.WriteByte(13); // Enter
+
+        for (byte i = 65; i < 123; i++)
+        {
+            fs.WriteByte(i);
+        }
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
